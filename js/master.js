54154b7afde3a8d3254a8ef7da2b2923efdf7eb2
Davide <davide.roberti89@gmail.com>
2020-02-20 16:14:25 +0100

    Fizz al posto dei numeri col resto di 3

--- js/master.js
+++ js/master.js
@@ -3,6 +3,12 @@
 // pongo una condizione: se sono multipli di 5 scrivi Buzz
 // pongo una condizione se sono multipli sia di 3 che di 5 scrivi FizzBuzz
 
+
 for (var i = 1; i < 101; i++) {
-    console.log(i);
+    restoTre = i % 3;
+    if (restoTre == 0) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    }
 }
