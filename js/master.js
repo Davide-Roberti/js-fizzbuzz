d42eccb0de969081cb9a70e680720a4cda1695fa
Davide <davide.roberti89@gmail.com>
2020-02-20 16:29:54 +0100

    Fine con stampa in html

--- js/master.js
+++ js/master.js
@@ -8,12 +8,16 @@ for (var i = 1; i < 101; i++) {
     restoTre = i % 3;
     restoCinque = i % 5;
     if (restoTre == 0 && restoCinque == 0) {
+        document.getElementById('elemento').innerHTML += '<li>FizzBuzz</li>';
         console.log('FizzBuzz');
     } else if (restoTre == 0) {
         console.log('Fizz');
+        document.getElementById('elemento').innerHTML += '<li>Fizz</li>';
     } else if (restoCinque == 0){
         console.log('Buzz');
+        document.getElementById('elemento').innerHTML += '<li>Buzz</li>';
     } else {
         console.log(i);
+        document.getElementById('elemento').innerHTML += '<li>' + i + '</li>';
     }
 }
