bfd83b225f5defea63142783423b9dda6cdf7902
Davide <davide.roberti89@gmail.com>
2020-02-20 15:45:22 +0100

    stampati numeri da 1 a 100

---
+++ js/master.js
@@ -0,0 +1,8 @@
+// stampo a schermo 100 numeri
+// pongo una condizione: se sono multipli di 3 scrivi Fizz
+// pongo una condizione: se sono multipli di 5 scrivi Buzz
+// pongo una condizione se sono multipli sia di 3 che di 5 scrivi FizzBuzz
+
+for (var i = 1; i < 101; i++) {
+    console.log(i);
+}
