551fbcf045c8ccb7d85d672b8cf1828dafa32e38
Davide <davide.roberti89@gmail.com>
2020-02-20 16:19:15 +0100

    Fine FizzBuzz

--- js/master.js
+++ js/master.js
@@ -6,8 +6,13 @@
 
 for (var i = 1; i < 101; i++) {
     restoTre = i % 3;
-    if (restoTre == 0) {
+    restoCinque = i % 5;
+    if (restoTre == 0 && restoCinque == 0) {
+        console.log('FizzBuzz');
+    } else if (restoTre == 0) {
         console.log('Fizz');
+    } else if (restoCinque == 0){
+        console.log('Buzz'); 
     } else {
         console.log(i);
     }
